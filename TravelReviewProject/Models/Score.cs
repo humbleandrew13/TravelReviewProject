4de4731ac6e839925823dc0e8d855f96e6171fc2
Andy Humble <humbleandrew13@gmail.com>
2017-02-10 14:27:15 -0500

    Added Score model

---
+++ TravelReviewProject/Models/Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewProject.Models
+{
+    public class Score
+    {
+        [Key]
+        public int ScoreID { get; set; }
+        public int ReviewScore { get; set; }
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
