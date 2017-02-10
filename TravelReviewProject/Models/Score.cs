69055b20d01eb809aba5690929fec17da520288c
Andy Humble <humbleandrew13@gmail.com>
2017-02-09 20:48:39 -0500

    Initial commit, almost forgot

---
+++ TravelReviewProject/Models/Score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewProject.Models
+{
+    public class Score
+    {
+        public int ScoreID { get; set;}
+        public int ReviewScore { get; set; }
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
